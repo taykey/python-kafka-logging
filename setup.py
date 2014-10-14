4ed8a1c15bc354f5848d7245c2f07f8b5e26f7eb
avihoo.mamka <avihoo@taykey.com>
2014-10-14 09:11:08 +0300

    Changed LICENSE to Apache 2.0 and changed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,48 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+import os
+import sys
+
+from codecs import open
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'python-kafka-logging',
+]
+
+requires = [ 'kafka-python==0.9.2', 'logstash_formatter==0.5.8', ]
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
-  name = 'python-kafka-logging',
-  packages = ['python-kafka-logging'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Simple python logging handler for forwarding logs to a kafka queue.',
-  author = 'Avihoo Mamka',
-  author_email = 'avihoo@taykey.com',
-  url = 'https://github.com/taykey/python-kafka-logging', # use the URL to the github repo
-  download_url = 'https://github.com/taykey/python-kafka-logging/tarball/0.1',
-  keywords = ['kafka', 'logging', 'python', 'logstash'], # arbitrary keywords
-  classifiers = [],
+    name='python-kafka-logging',
+    version=0.1,
+    description='Simple python logging handler for forwarding logs to a kafka queue.',
+    long_description=readme + '\n\n',
+    author='Avihoo Mamka',
+    author_email='avihoo@taykey.com',
+    url='https://github.com/taykey/python-kafka-logging',
+    packages=packages,
+    package_data={'': ['LICENSE.txt']},
+    include_package_data=True,
+    install_requires=requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+
+    ),
 )
